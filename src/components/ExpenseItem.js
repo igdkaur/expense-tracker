92ccc08bf92f07a8c3227505d823a124fac56851
igdkaur <igdkaur@gmail.com>
2021-12-31 22:11:41 +0530

    added css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 const ExpenseItem = (props) => {
-
-
   return (
-    <div>
-      <div>{props.title}</div>
+    <div className=" expense-item">
       <div>{props.expenseDate.toISOString()}</div>
-      <div>{props.amount}</div>
+      <div className=" expense-item__description">
+        <h2> {props.title}</h2>
+      </div>
+      <div className=" expense-item__price">{props.amount}</div>
     </div>
   );
 };
 
 export default ExpenseItem;
 
-// now move expense to app.js as an array of objects and pass it to expense item 
+// now move expense to app.js as an array of objects and pass it to expense item
