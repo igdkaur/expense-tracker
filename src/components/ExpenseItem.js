e55ed33038a9a1ac38061c65be8f9fd2b1fb4fb0
igdkaur <igdkaur@gmail.com>
2021-12-31 22:24:16 +0530

    separate component and style it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   return (
     <div className=" expense-item">
-      <div>{props.expenseDate.toISOString()}</div>
+      <ExpenseDate date = {props.expenseDate}/>
       <div className=" expense-item__description">
         <h2> {props.title}</h2>
       </div>
