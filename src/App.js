71ebe79d5708f314fa3cf6933e92ce7ef921cdee
igdkaur <igdkaur@gmail.com>
2021-12-31 22:02:30 +0530

    map the expenses to pass as props

--- src/App.js
+++ src/App.js
@@ -25,28 +25,11 @@ const expenses = [
 function App() {
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        expenseDate={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        expenseDate={expenses[1].date}
-      />
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        expenseDate={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        expenseDate={expenses[3].date}
-      /> 
-     
+      {expenses.map(e => <ExpenseItem
+        title={e.title}
+        amount={e.amount}
+        expenseDate={e.date}
+      />)}     
     </div>
   );
 }
