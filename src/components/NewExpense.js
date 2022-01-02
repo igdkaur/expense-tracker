364c4a947ea141808fafbe1a2d1caafcd8c6a063
igdkaur <igdkaur@gmail.com>
2022-01-02 15:57:33 +0530

    lift the state changes

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -6,10 +6,20 @@ import './NewExpense.css';
 
 
 const NewExpense = (props) => {
+
+const handleSave = (enteredExpenseData) => {
+  const expenseData = { ...enteredExpenseData, 
+  id:Math.random().toString()
+}
+props.onAddExpense (expenseData);
+
+
+}
+
   
   return (
     <div className='new-expense'>
-      <ExpenseForm onSave = {props.onAddExpense}/>
+      <ExpenseForm onSave = {handleSave}/>
     </div>
   );
 };
